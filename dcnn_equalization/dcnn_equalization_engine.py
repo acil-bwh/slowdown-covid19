95bf9c6c7dfa58ef7cd3109f5b0404c59ec38151
PietroNardelli <pnardelli@bwh.harvard.edu>
2020-04-02 18:55:49 -0400

    ENH: small changes

--- dcnn_equalization/dcnn_equalization_engine.py
+++ dcnn_equalization/dcnn_equalization_engine.py
@@ -35,9 +35,9 @@ class COVID19EqualizationNetwork(Network):
 
         self._expected_input_values_range_ = (0.0, 1.0)
 
-    def conv2d(self, input_layer, filters=32, kernel_size=3, batch_normalization=True):
+    def conv2d(self, input_layer, filters=32, kernel_size=3, batch_normalization=True, activation='relu'):
         layer = tf.keras.layers.Conv2D(filters, kernel_size=kernel_size, strides=1,
-                                       activation='relu', padding='same')(input_layer)
+                                       activation=activation, padding='same')(input_layer)
         if batch_normalization:
             layer = tf.keras.layers.BatchNormalization()(layer)
         return layer
@@ -46,32 +46,32 @@ class COVID19EqualizationNetwork(Network):
         inputs = tf.keras.layers.Input(self.target_image_size)
 
         # Part 1
-        conv1 = self.conv2d(inputs, filters=32, kernel_size=3, batch_normalization=True)
-        conv2 = self.conv2d(conv1, filters=32, kernel_size=3, batch_normalization=True)
-        conv3 = self.conv2d(conv2, filters=32, kernel_size=3, batch_normalization=True)
+        conv1 = self.conv2d(inputs, filters=12, kernel_size=3, batch_normalization=True)
+        conv2 = self.conv2d(conv1, filters=12, kernel_size=3, batch_normalization=True)
+        conv3 = self.conv2d(conv2, filters=12, kernel_size=3, batch_normalization=True)
 
         # Part 2 (Down-sampling)
         pool1 = tf.keras.layers.MaxPool2D(pool_size=(2, 2))(conv3)
-        conv4 = self.conv2d(pool1, filters=64, kernel_size=3, batch_normalization=True)
-        conv5 = self.conv2d(conv4, filters=64, kernel_size=3, batch_normalization=True)
-        conv6 = self.conv2d(conv5, filters=64, kernel_size=3, batch_normalization=True)
+        conv4 = self.conv2d(pool1, filters=24, kernel_size=3, batch_normalization=True)
+        conv5 = self.conv2d(conv4, filters=24, kernel_size=3, batch_normalization=True)
+        conv6 = self.conv2d(conv5, filters=24, kernel_size=3, batch_normalization=True)
 
         # Part 3 (Up-sampling)
         up1 = tf.keras.layers.UpSampling2D(size=(2, 2))(conv6)
         add1 = tf.keras.layers.concatenate([up1, conv3], axis=-1)
 
         # Part 4
-        conv7 = self.conv2d(add1, filters=32, kernel_size=3, batch_normalization=True)
-        conv8 = self.conv2d(conv7, filters=32, kernel_size=3, batch_normalization=True)
-        conv9 = self.conv2d(conv8, filters=32, kernel_size=3, batch_normalization=True)
+        conv7 = self.conv2d(add1, filters=12, kernel_size=3, batch_normalization=True)
+        conv8 = self.conv2d(conv7, filters=12, kernel_size=3, batch_normalization=True)
+        conv9 = self.conv2d(conv8, filters=12, kernel_size=3, batch_normalization=True)
 
         # Part 5
-        conv10 = self.conv2d(inputs, filters=32, kernel_size=3, batch_normalization=True)
-        conv11 = self.conv2d(conv10, filters=32, kernel_size=3, batch_normalization=True)
-        conv12 = self.conv2d(conv11, filters=32, kernel_size=3, batch_normalization=True)
+        conv10 = self.conv2d(inputs, filters=12, kernel_size=3, batch_normalization=True)
+        conv11 = self.conv2d(conv10, filters=12, kernel_size=3, batch_normalization=True)
+        conv12 = self.conv2d(conv11, filters=12, kernel_size=3, batch_normalization=True)
 
         add2 = tf.keras.layers.concatenate([conv9, conv12], axis=-1)
-        conv13 = self.conv2d(add2, filters=1, kernel_size=3, batch_normalization=True)
+        conv13 = self.conv2d(add2, filters=1, kernel_size=3, batch_normalization=False, activation='sigmoid')
 
         return tf.keras.models.Model(inputs=inputs, outputs=conv13)
 
@@ -82,24 +82,24 @@ def train(h5_dataset_file, output_folder):
     X_v = HDF5Matrix(h5_dataset_file, 'X_val')
     y_v = HDF5Matrix(h5_dataset_file, 'y_val')
 
-    path = output_folder + '/equalization_model-{epoch:03d}-{acc:03f}-{val_acc:03f}.h5'
+    path = output_folder + '/equalization_model-{epoch:03d}-{mean_absolute_error:03f}-{val_mean_absolute_error:03f}.h5'
 
     model_checkpoint = callbacks.ModelCheckpoint(path, verbose=1, monitor='val_loss', save_best_only=False, mode='auto')
     earlyStopping = callbacks.EarlyStopping(monitor='val_loss', patience=10, verbose=0, mode='min')
 
     network = COVID19EqualizationNetwork(target_image_size=(1024,1024))
-    network.build_model(True, optimizer=tf.keras.optimizers.Adam(lr=1e-4),
+    network.build_model(True, optimizer=tf.keras.optimizers.Adam(lr=1e-5),
                         loss_function='mse', additional_metrics=['mae'],
                         pretrained_weights_file_path=None)
     model = network.model
-    history = model.fit(X_t, y_t, shuffle=False, batch_size=80, epochs=100, validation_data=(X_v, y_v),
+    history = model.fit(X_t, y_t, shuffle=False, batch_size=3, epochs=100, validation_data=(X_v, y_v),
                         callbacks=[model_checkpoint, earlyStopping], verbose=1, max_queue_size=100)
 
     final_path = output_folder + 'equalization-last_model.h5'
     model.save(final_path, overwrite=True)
 
-    acc = history.history['mae']
-    val_acc = history.history['val_mae']
+    acc = history.history['mean_absolute_error']
+    val_acc = history.history['val_mean_absolute_error']
     loss = history.history['loss']
     val_loss = history.history['val_loss']
 
@@ -131,7 +131,6 @@ if __name__ == "__main__":
     parser.add_argument('--i', help="Input h5 dataset file", type=str, required=True)
     parser.add_argument('--o', help="Output folder", type=str, required=False)
     # parser.add_argument('--m', help="Model file to use for validation/testing",type=str)
-    # parser.add_argument('--out_csv', help="Prediction results (csv format). Use in testing or validation")
 
     args = parser.parse_args()
     if args.operation == 'TRAIN':
